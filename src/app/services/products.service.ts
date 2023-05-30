70b35d6dc9f23a738b4bcec06fde7fa82334f2bd
Andres Norato <carnorat@gmail.com>
2023-05-30 17:24:04 -0500

    manejo de ambientes en angular

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CreateProductDTO, Product,UpdateProductDTO } from '../models/product.model';
 import { retry, retryWhen } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'api/products'
+  private apiUrl = `${environment.API_URL}/products`
 
   constructor(
     private http: HttpClient
