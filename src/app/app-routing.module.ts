ee86afe12e7d25bbe74e5966fe2b58e81531bac2
Andres Norato <carnorat@gmail.com>
2023-06-08 09:55:56 -0500

    precarga de modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CustomPreloadService } from './services/custom-preload.service';
 
 const routes: Routes = [
   {
      path: '', 
-     loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
+     loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule),
+     data: {
+       preload: true
+     }
   },
   {
     path: 'cms',
-    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
+    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule),
+    data: {
+      preload: false
+    }
   },
   {
     path: '**',
@@ -18,7 +25,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: CustomPreloadService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
